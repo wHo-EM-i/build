5e936e04eb5a0733b603c0655410131a33968103
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add CAF-EX build support
    Squashed also with the following changes:
    
      Author: Luca Stefani <luca020400@lineageos.org>
      Date:   Thu Aug 17 22:22:13 2017 +0200
    
        core: Include Aosp pathmap
    
      Change-Id: Ie25dee383cc5bc9bb6390cff2cb2460d526d80b6
    
      Author: Simon Shields <simon@lineageos.org>
      Date:   Fri Mar 2 12:55:47 2018 +1100
    
        build: unconditionally use AOSP pathmap
    
        even if we're not building a aosp target, we need to
        use the aosp pathmap to make things like recovery and ril
        happy
    
      Change-Id: I974c30ad10d4ff5b7805c3df9c22010f1e002bf1
    
      Author: Luca Stefani <luca.stefani.ge1@gmail.com>
      Date:   Tue Jan 22 15:50:52 2019 +0100
    
        Don't export BUILD_NUMBER in envsetup
    
        * The value is meant to be set only before running an /official/ build
        * This allows us to execute consecutive make commands without
          running kati for every invocation
    
      Change-Id: Ifa94b839f30c7260009c8a3c91c202b50e28022e
    
      Author: Luca Stefani <luca020400@lineageos.org>
      Date:   Fri Aug 18 10:17:00 2017 +0200
    
        envsetup: Call fixup_common_out_dir
    
    Change-Id: I204c2e5a175f92e4d266a8a3ee7c10c89a5fe270

--- core/config.mk
+++ core/config.mk
@@ -319,6 +319,10 @@ include $(BUILD_SYSTEM)/envsetup.mk
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
+ifneq ($(EXTENDED_BUILD),)
+include vendor/extended/config/BoardConfigAosp.mk
+endif
+
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
@@ -1157,6 +1161,12 @@ endif
 dont_bother_goals := out \
     product-graph dump-products
 
+ifneq ($(EXTENDED_BUILD),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+#$(eval include device/extended/sepolicy/common/sepolicy.mk)
+endif
+
 # Make ANDROID Soong config variables visible to Android.mk files, for
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
@@ -1173,5 +1183,8 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 
 # Make RECORD_ALL_DEPS readonly.
 RECORD_ALL_DEPS :=$= $(filter true,$(RECORD_ALL_DEPS))
+
+# Include any vendor specific config.mk file
+-include vendor/*/build/core/config.mk
 
 include $(BUILD_SYSTEM)/dumpvar.mk
